8a4796d62634c0e678511d0760685a0bbf7ae707
asleepandy <asleepandy@gmail.com>
2015-03-05 14:37:17 +0800

    TestCase/GradlePractice

---
+++ TestCase/GradlePractice/src/main/java/com/wavegis/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.wavegis;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+ 
+public class HelloWorld {
+ 
+    static Log logger = LogFactory.getLog(HelloWorld.class);
+ 
+    public static void main(String[] args) {
+        logger.info("Hello World");
+    }
+ 
+}
